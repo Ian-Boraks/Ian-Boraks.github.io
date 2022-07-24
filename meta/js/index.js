2cb6848364de23f62946a523aea83b0f5018d41e
IanBoraks <ianboraks@gmail.com>
2022-07-24 09:22:55 -0400

    Added in JS mouse following

--- meta/js/index.js
+++ meta/js/index.js
@@ -1,4 +1,54 @@
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
+
+// }, false);
+
+var JSSwitch = true;
+
+var mouse_monitor = function (e) {
+  if (location.href.includes('#JS')) {
+    var x = e.pageX;
+    var y = e.pageY;
+
+    var positionX = (x / window.innerWidth) * 17;
+    var positionY = (y / window.innerHeight) * 17;
+
+    document.getElementById('name_content').style.setProperty('--positionX', positionX);
+    document.getElementById('name_content').style.setProperty('--positionY', positionY);
+  }
+}
+
+$(document).ready(function () {
+  $(this).on('mousemove', mouse_monitor);
+
+  $("#name_description_button").click(function () {
+    if (location.href.includes('#JS')) {
+      location.href = location.href.replace('#JS', '#CSS');
+      document.querySelectorAll('.cell').forEach(cell => {
+        cell.style.zIndex = '2';
+      });
+      document.getElementById('name_description_content').textContent = "Mouse following is done 100% with CSS no JS";
+      document.getElementById('name_description_button').textContent = "CLICK ME TO FOLLOW WITH JS";
+
+      document.getElementById('name_content').style.removeProperty('--positionX');
+      document.getElementById('name_content').style.removeProperty('--positionY');
+    } else if (location.href.includes('#CSS')) {
+      location.href = location.href.replace('#CSS', '#JS');
+      document.querySelectorAll('.cell').forEach(cell => {
+        cell.style.zIndex = '0';
+      });
+      document.getElementById('name_description_content').textContent = "Mouse following is done with JS";
+      document.getElementById('name_description_button').textContent = "CLICK ME TO FOLLOW WITH CSS";
+    } else {
+      location.href = location.href + '#CSS';
+    }
+  });
+
+  if (!location.href.includes('#CSS') ? !location.href.includes('#JS') : location.href.includes('#JS')) {
+    location.href = location.href + '#CSS';
+  } else if (location.href.includes('#JS')) {
+    document.getElementById('name_description_button').click();
+  }
+
   for (let i = 0; i < 289; i++) {
     $('#name').append('<div class="cell"></div>');
   }
@@ -12,4 +62,4 @@ document.addEventListener('DOMContentLoaded', function () {
       $('#name_content').append('<div class="name_followers">Ian Boraks</div>');
     }
   }
-}, false);
+});
